585e640fb95190cae16b7234eab33cf7458af449
Eva Marques <marquesel@cn040603.hpc.niehs.nih.gov>
2024-03-27 14:44:01 -0400

    update UT for double_to_site_id() function

--- tests/testthat/test-create_xgboost_data.R
+++ tests/testthat/test-create_xgboost_data.R
@@ -6,7 +6,10 @@ testthat::test_that("double_to_site_id function is working well", {
   expect_no_error(double_to_site_id(x = 12))
   expect_error(double_to_site_id(x = 123456789123456), "too many digits")
   expect_equal(double_to_site_id(1), "00000000000001")
-  expect_error(double_to_site_id("1"), )
+  expect_error(double_to_site_id("1"),
+               "non-numeric argument to mathematical function")
+  expect_error(double_to_site_id(c(1,1)),
+               "the condition has length > 1")
 })
 
 #' @author Anisha Singh
